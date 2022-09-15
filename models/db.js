f16f2bbc8e09607afa4ab76a857131fe7f9bfd8d
j52 <j52xue@uwaterloo.ca>
2022-09-15 00:10:46 -0400

    deploying

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoClient = require('mongodb').MongoClient;
 
-const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017';
+const connectionString = process.env.MONGO_URI || 'mongodb://0.0.0.0:27017';
 
 const littleLegendsCollectionName = 'littleLegends';
 const eggsCollectionName = 'eggs';
